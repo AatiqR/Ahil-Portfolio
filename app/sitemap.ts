b1f4f3c9b0e7c6bbc604de80f9088644495f65e5
Ahil Rana <146585739+AatiqR@users.noreply.github.com>
2025-10-24 02:02:11 +0500

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://ahilwebstudio.vercel.app/",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1.0,
+    },
+    {
+      url: "https://ahilwebstudio.vercel.app/services",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: "https://ahilwebstudio.vercel.app/projects",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: "https://ahilwebstudio.vercel.app/contact",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}
