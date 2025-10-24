f32cfd0e818034a52b2c04a795cccc857907cd75
Ahil Rana <146585739+AatiqR@users.noreply.github.com>
2025-10-25 00:33:32 +0500

    robots fix

--- app/robots.ts
+++ app/robots.ts
@@ -4,13 +4,11 @@ export default function robots(): MetadataRoute.Robots {
   const baseUrl = "https://ahilwebstudio.vercel.app";
 
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-        disallow: ["/api/", "/private/"],
-      },
-    ],
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/private",
+    },
     sitemap: `${baseUrl}/sitemap.xml`,
     host: baseUrl,
   };
