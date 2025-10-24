098665426b00ea795bead80b4c504c32cd8a2f4e
Ahil Rana <146585739+AatiqR@users.noreply.github.com>
2025-10-24 23:57:48 +0500

    Final SItemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,21 +1,33 @@
 import { MetadataRoute } from "next";
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://ahilwebstudio.vercel.app";
 
-  const routes = [
-    "",
-    "/services",
-    "/projects",
-    "/contact",
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/services`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/projects`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date().toISOString(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
   ];
-
-  const sitemap = routes.map((route) => ({
-    url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString(),
-    changeFrequency: "monthly",
-    priority: route === "" ? 1.0 : 0.9,
-  }));
-
-  return sitemap;
 }
+
